11acec719a009ef605f85846fd7fcf8669fd0d13
Patrick Werner <boon.werner@gmail.com>
2020-01-10 15:38:11 +0100

    fix connection missing in abstract stack machine visitorw fix nxt bug fix ardu rfid fix random element from list in cpp fix smaller ev3 lejos bugs

--- OpenRobertaRobot/src/main/java/de/fhg/iais/roberta/visitor/lang/codegen/prog/AbstractJavaVisitor.java
+++ OpenRobertaRobot/src/main/java/de/fhg/iais/roberta/visitor/lang/codegen/prog/AbstractJavaVisitor.java
@@ -283,7 +283,7 @@ public abstract class AbstractJavaVisitor extends AbstractLanguageVisitor {
 
     @Override
     public Void visitListRepeat(ListRepeat<Void> listRepeat) {
-        this.sb.append("new ArrayList<>(Collections.nCopies(");
+        this.sb.append("new ArrayList<>(Collections.nCopies( (int) ");
         listRepeat.getParam().get(1).accept(this);
         this.sb.append(", ");
         if ( listRepeat.getParam().get(0).getVarType() == BlocklyType.NUMBER ) {
